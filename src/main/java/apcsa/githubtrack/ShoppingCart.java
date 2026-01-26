c0802ee69fca44714cf122bf9dc24976be94e846
Abaas Hamdani <saimajafri110786@gmail.com>
2026-01-26 20:23:08 +0000

    Your commit message here

--- src/main/java/apcsa/githubtrack/ShoppingCart.java
+++ src/main/java/apcsa/githubtrack/ShoppingCart.java
@@ -1,3 +1,102 @@
 package apcsa.githubtrack;
 
 // Implement your ShoppingCart class here
+/*## 🛒 Class 3: `ShoppingCart`
+
+### Purpose
+
+Represents a shopper’s cart.
+
+### Required Attributes
+
+* Associated `ShoppingList`
+* Customer name
+* Whether or not the shopping mission is complete
+* **Total** number of `ShoppingCarts` returned
+
+### Constructors
+
+* If no `ShoppingList` provided:
+
+  * Initialize with an empty `ShoppingList`
+
+---
+
+### Required Methods
+
+#### `isCompleted()`
+
+* Returns `true` only if **all items in the `ShoppingList` are bought**
+
+#### `returnCart()`
+
+* Can only return a cart if:
+
+  * The associated shopping list is completed
+* Once returned:
+
+  * Counts toward total returned carts
+  * Cannot be returned again
+
+#### Accessors
+
+* All attributes must be accessible outside the class
+
+---
+*/ 
+public class ShoppingCart {
+    private ShoppingList shoppingList;
+    private String customerName;
+    private boolean isCompleted;
+    private boolean isReturned;
+    private static int totalReturnedCarts = 0;
+
+    
+    public ShoppingCart(String customerName) {
+        this.shoppingList = new ShoppingList(); 
+        this.customerName = customerName;
+        this.isCompleted = false;
+        this.isReturned = false;
+    }
+
+    public ShoppingCart(ShoppingList shoppingList, String customerName) {
+        this.shoppingList = shoppingList;
+        this.customerName = customerName;
+        this.isCompleted = false;
+        this.isReturned = false;
+    }
+
+    public boolean isCompleted() {
+        for (int i = 0; i < this.shoppingList.getStorage().length; i++) {
+            if (this.shoppingList.getStorage()[i] != null) {
+                if (!this.shoppingList.getStorage()[i].isSold()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void returnCart() {
+        if (isCompleted() && !isReturned) {
+            totalReturnedCarts++;
+            isReturned = true;
+        }
+    }
+
+    public ShoppingList getShoppingList() {
+        return shoppingList;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public boolean getIsCompleted() {
+        return isCompleted();
+    }
+
+    public static int getTotalReturnedCarts() {
+        return totalReturnedCarts;
+    }
+}
--- src/main/java/apcsa/githubtrack/ShoppingItem.java
+++ src/main/java/apcsa/githubtrack/ShoppingItem.java
@@ -1,3 +1,96 @@
 package apcsa.githubtrack;
 
 // Implement your ShoppingItem class here
+/*## 🧺 Class 1: `ShoppingItem`
+
+### Purpose
+
+Represents a single item in a store.
+
+### Required Attributes
+
+* Item name
+* Item price (stored **in pennies**)
+* Whether or not the item is sold
+
+### Required Behaviors
+
+#### `buy()`
+
+* Marks the item as sold
+* If already sold, nothing changes
+* Once sold, **cannot be “unbought”**
+
+#### `equals(ShoppingItem other)`
+
+* Returns `true` **if and only if**:
+
+  * The item's `name` is the same as `other`'s
+  * The item's `price` is the same as `other`'s 
+
+#### `compareTo(ShoppingItem other)`
+
+* If names are different:
+
+  * Print: `"Error: Different items!"`
+* If names are the same:
+
+  * Return `0` if prices are equal
+  * Return a **positive difference** (magnitude equal to the price difference) if this item is more expensive 
+  * Return a **negative difference** (magnitude equal to the price difference) if this item is cheaper
+
+#### Accessor Methods
+
+* All attributes must be able to be accessed outside the class
+* The **price getter must return a `double` in dollars**
+*/
+public class ShoppingItem {
+    private String name;
+    private int pennies;
+    private boolean sold;
+    public ShoppingItem(String name, int pennies) {
+        this.name = name;
+        this.pennies = pennies;
+        this.sold = false;
+    }
+    public void buy() {
+        this.sold = true;
+    }
+    public boolean equals(ShoppingItem other) {
+        if (other == null) {
+            return false;
+        }
+        return this.name.equals(other.name) && this.pennies == other.pennies;   
+    }
+    public int compareTo(ShoppingItem other) {
+        if (!this.name.equals(other.name)) {
+            System.out.println("Error: Different items!");
+        
+        } 
+        if (this.name.equals(other.name)){
+            if (this.pennies == other.pennies) {
+                return 0;
+            } else if (this.pennies > other.pennies) {
+                return other.pennies - this.pennies;
+            } else {
+                return this.pennies - other.pennies;
+            }
+
+
+
+        }
+        return 0;
+    }
+    public String getName() {
+        if (this.name == null) {
+            return "";
+        }
+        return this.name;
+    }
+    public double getPrice() {
+        return this.pennies / 100.0;
+    }
+    public boolean isSold() {
+        return this.sold;
+    }
+}
